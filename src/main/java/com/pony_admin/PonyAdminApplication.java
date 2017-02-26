4dc8daaa147c8705e4f9f9f2f898235e433adfe5
qiaoyi <qiaoyi@meituan.com>
2017-02-26 22:08:27 +0800

    init project

---
+++ src/main/java/com/pony_admin/PonyAdminApplication.java
@@ -0,0 +1,12 @@
+package com.pony_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonyAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PonyAdminApplication.class, args);
+	}
+}
